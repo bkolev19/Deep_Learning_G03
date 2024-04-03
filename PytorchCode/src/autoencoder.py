bb70fa95082a767bb130e4a9c072a2c65208ee4e
POTN1K <rickeroliver@gmail.com>
2024-04-03 14:49:52 +0200

    Small change to autoencoder

--- PytorchCode/src/autoencoder.py
+++ PytorchCode/src/autoencoder.py
@@ -2,9 +2,7 @@
 
 # Libraries
 import torch
-from itertools import combinations
 from scipy.special import binom
-import numpy as np
 
 
 class Autoencoder(torch.nn.Module):
@@ -293,7 +291,6 @@ def sindy_library_torch(z, latent_dim, poly_order, include_sine=False):
 
     return torch.stack(library, dim=1)
 
-
 def sindy_library_torch_order2(z, dz, latent_dim, poly_order, include_sine=False):
     """
     Build the SINDy library for a second order system. This is essentially the same as for a first
@@ -337,59 +334,3 @@ def sindy_library_torch_order2(z, dz, latent_dim, poly_order, include_sine=False
             library.append(torch.sin(z_combined[:,i]))
 
     return torch.stack(library, dim=1)
-
-
-if __name__ == '__main__':
-    # Params
-    params = {'activation': 'sigmoid',
-            'batch_size': 1,
-            'coefficient_initialization': 'constant',
-            'coefficient_mask': np.array([[False,  True, False],
-                [False, False, False],
-                [False,  True, False],
-                [ True, False,  True],
-                [False,  True, False],
-                [ True, False,  True],
-                [False, False, False],
-                [False, False, False],
-                [ True, False,  True],
-                [False,  True, False],
-                [False, False, False],
-                [False, False, False],
-                [False, False, False],
-                [False, False, False],
-                [False, False, False],
-                [False, False, False],
-                [False, False, False],
-                [False, False, False],
-                [False, False, False],
-                [False, False, False]]),
-            'coefficient_threshold': 0.1,
-            'epoch_size': 512000,
-            'include_sine': False,
-            'input_dim': 8,
-            'latent_dim': 2,
-            'learning_rate': 0.001,
-            'library_dim': 20,
-            'loss_weight_decoder': 1.0,
-            'loss_weight_sindy_regularization': 1e-05,
-            'loss_weight_sindy_x': 0.0001,
-            'loss_weight_sindy_z': 0.0,
-            'max_epochs': 10001,
-            'model_order': 1,
-            'poly_order': 2,
-            'print_frequency': 100,
-            'print_progress': True,
-            'refinement_epochs': 1001,
-            'sequential_thresholding': False,
-            'threshold_frequency': 500,
-            'widths': [4, 3]}
-    
-    model = Autoencoder(params)
-
-    # DATA
-    x = torch.rand(1, 8)
-    dx = torch.rand(1, 8)
-    
-    # Forward pass
-    model.forward([x, dx])
