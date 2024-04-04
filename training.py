a88c3bd7772fe6e8459156d9aff748ea69f0f403
avatarjasper <93929084+avatarjasper@users.noreply.github.com>
2024-04-04 10:20:21 +0200

    made some things torch instead of numpy

--- training.py
+++ training.py
@@ -7,21 +7,36 @@ from autoencoder import Autoencoder
     
 batch_size = 32
 
+
+def create_feed_dictionary(data, params, idxs=None):
+    if idxs is None:
+        idxs = np.arange(data['x'].shape[0])
+    feed_dict = {}
+    feed_dict['x:0'] = torch.tensor(data['x'][idxs], dtype=torch.float32)
+    feed_dict['dx:0'] = torch.tensor(data['dx'][idxs], dtype=torch.float32)
+    if params['model_order'] == 2:
+        feed_dict['ddx:0'] = torch.tensor(data['ddx'][idxs], dtype=torch.float32)
+    if params['sequential_thresholding']:
+        feed_dict['coefficient_mask:0'] = torch.tensor(params['coefficient_mask'], dtype=torch.float32)
+    feed_dict['learning_rate:0'] = torch.tensor(params['learning_rate'], dtype=torch.float32)
+    return feed_dict
+
+
 def train_network(training_data, val_data, params):
     model       = Autoencoder(params)
     optimizer   = torch.optim.Adam(model.parameters(), 0.001)
     batch_iter  = params['epoch_size']//params['batch_size']
 
     validation_dict   = create_feed_dictionary(val_data, params, idxs=None)
-    x_norm            = np.mean(val_data['x']**2)
+    x_norm            = torch.mean(val_data['x']**2)
     
     if params['model_order'] == 1:
-        sindy_predict_norm_x = np.mean(val_data['dx']**2)
+        sindy_predict_norm_x = torch.mean(val_data['dx']**2)
     else:
-        sindy_predict_norm_x = np.mean(val_data['ddx']**2)
+        sindy_predict_norm_x = torch.mean(val_data['ddx']**2)
 
     validation_losses = []
-    sindy_model_terms = [np.sum(params['coefficient_mask'])]
+    sindy_model_terms = [torch.sum(params['coefficient_mask'])]
 
 
 
@@ -39,7 +54,7 @@ def train_network(training_data, val_data, params):
             optimizer.step()
 
         if params['print_progress'] and (i % params['print_frequency'] == 0):
-                validation_losses.append(model.loss_func())
+            validation_losses.append(model.loss_func())
 
         if params['sequential_thresholding'] and (i % params['threshold_frequency'] == 0) and (i > 0):
             params['coefficient_mask'] = np.abs(model.sindy_coefficients) > params['coefficient_threshold']
@@ -52,9 +67,9 @@ def train_network(training_data, val_data, params):
     torch.save(model.state_dict(), MODEL_PATH)
 
     if params['model_order'] == 1:
-        sindy_predict_norm_z = np.mean((model.dz, feed_dict=validation_dict)**2)
+        sindy_predict_norm_z = torch.mean((model.dz(validation_dict['dz'])**2)) 
     else:
-        sindy_predict_norm_z = np.mean((model.ddz, feed_dict=validation_dict)**2)
+        sindy_predict_norm_z = torch.mean((model.ddz(validation_dict['ddz']))**2)
     sindy_coefficients = model.sindy_coefficients
     
 
@@ -69,16 +84,3 @@ def train_network(training_data, val_data, params):
     results_dict['sindy_model_terms'] = np.array(sindy_model_terms)
 
     return results_dict
-
-def create_feed_dictionary(data, params, idxs=None):
-    if idxs is None:
-        idxs = np.arange(data['x'].shape[0])
-    feed_dict = {}
-    feed_dict['x:0'] = torch.tensor(data['x'][idxs], dtype=torch.float32)
-    feed_dict['dx:0'] = torch.tensor(data['dx'][idxs], dtype=torch.float32)
-    if params['model_order'] == 2:
-        feed_dict['ddx:0'] = torch.tensor(data['ddx'][idxs], dtype=torch.float32)
-    if params['sequential_thresholding']:
-        feed_dict['coefficient_mask:0'] = torch.tensor(params['coefficient_mask'], dtype=torch.float32)
-    feed_dict['learning_rate:0'] = torch.tensor(params['learning_rate'], dtype=torch.float32)
-    return feed_dict
